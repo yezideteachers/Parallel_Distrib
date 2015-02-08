dd4cadce88ff6d81e35553620cef3623e2db53f6
yezideteachers <ob005696@etu.unice.fr>
2015-02-08 23:25:15 +0100

    exo1 Parallel_distr

--- TP/TP1/Trieur.java
+++ TP/TP1/Trieur.java
@@ -1,13 +1,24 @@
+package tp1;
+
 /**
  * Tri d'un tableau d'entiers multi-thread.
  * Utilisation de wait() et notifyAll() au lieu de join()
  */
+ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+import javax.print.DocFlavor.STRING;
+
 public class Trieur extends Thread {
 
-  private int[] t; // tableau à trier
+  private int[] t; // tableau Ã  trier
   private int debut, fin; // tranche de ce tableau qu'il faut trier
-  private Trieur parent;  // tread Trieur qui a lancé ce (this) Trieur
-  private int nbNotify = 0; // Nombre de notifys envoyés à ce (this) Trieur
+  private Trieur parent;  // tread Trieur qui a lancÃ© ce (this) Trieur
+  private int nbNotify = 0; // Nombre de notifys envoyÃ©s Ã  ce (this) Trieur
+  private static int compt = 0;
 
   public Trieur(int[] t) {
     this(null, t, 0, t.length - 1);
@@ -18,17 +29,21 @@ public class Trieur extends Thread {
     this.t = t;
     this.debut = debut;
     this.fin = fin;
+   
     start();
+   
+   
+   
   }
 
   public synchronized void notifier() {
     // Notifie tous les thread en attente sur "ce" (this) moniteur
-    // Attention, quand le message sera envoyé au parent (dans run()),
-    // on incrémentera la variable nbNotify du parent (qui sera le this
+    // Attention, quand le message sera envoyÃ© au parent (dans run()),
+    // on incrÃ©mentera la variable nbNotify du parent (qui sera le this
     // implicite) et on notifiera le parent.
-    // On peut aussi ne pas utiliser de méthode mais dans ce cas, il faut
-    // écrire parent.nbNotify++; parent.notifyAll(); dans un bloc 
-    // synchronisé sur parent placé dans la méthode run (à la place de
+    // On peut aussi ne pas utiliser de mÃ©thode mais dans ce cas, il faut
+    // Ã©crire parent.nbNotify++; parent.notifyAll(); dans un bloc 
+    // synchronisÃ© sur parent placÃ© dans la mÃ©thode run (Ã  la place de
     // "parent.notifier()").
     this.nbNotify++;
     this.notifyAll();
@@ -44,10 +59,12 @@ public class Trieur extends Thread {
       int milieu = debut + (fin - debut) / 2;
       Trieur trieur1 = new Trieur(this, t, debut, milieu);
       Trieur trieur2 = new Trieur(this, t, milieu + 1, fin);
+      System.out.print("compt :"+compt+" , ");
+      compt=compt+1;
       // attend les 2 threads
       synchronized(this) {
         try {
-	  // Tant que 2 notifications n'ont pas été reçues (1 par
+	  // Tant que 2 notifications n'ont pas Ã©tÃ© reÃ§ues (1 par
 	  // trieur "fils"), on attend.
           while (nbNotify < 2) {
             wait();
@@ -72,20 +89,20 @@ public class Trieur extends Thread {
   }
 
   /**
-   * Fusionne 2 tranches déjà triées du tableau t.
-   *   - 1ère tranche : de debut à milieu = (debut + fin) / 2
-   *   - 2ème tranche : de milieu + 1 à fin
-   * @param debut premier indice de la 1ère tranche
-   * @param fin dernier indice de la 2ème tranche
+   * Fusionne 2 tranches dÃ©jÃ  triÃ©es du tableau t.
+   *   - 1Ã¨re tranche : de debut Ã  milieu = (debut + fin) / 2
+   *   - 2Ã¨me tranche : de milieu + 1 Ã  fin
+   * @param debut premier indice de la 1Ã¨re tranche
+   * @param fin dernier indice de la 2Ã¨me tranche
    */
   private void triFusion(int debut, int fin) {
-    // tableau où va aller la fusion
+    // tableau oÃ¹ va aller la fusion
     int[] tFusion = new int[fin - debut + 1];
     int milieu = (debut + fin) / 2;
-    // Indices des éléments à comparer
+    // Indices des Ã©lÃ©ments Ã  comparer
     int i1 = debut, 
         i2 = milieu + 1;
-    // indice de la prochaine case du tableau tFusion à remplir
+    // indice de la prochaine case du tableau tFusion Ã  remplir
     int iFusion = 0;
     while (i1 <= milieu && i2 <= fin) {
       if (t[i1] < t[i2]) {
@@ -96,13 +113,13 @@ public class Trieur extends Thread {
       }
     }
     if (i1 > milieu) {
-      // la 1ère tranche est épuisée
+      // la 1Ã¨re tranche est Ã©puisÃ©e
       for (int i = i2; i <= fin; ) {
         tFusion[iFusion++] = t[i++];
       }
     }
     else {
-      // la 2ème tranche est épuisée
+      // la 2Ã¨me tranche est Ã©puisÃ©e
       for (int i = i1; i <= milieu; ) {
         tFusion[iFusion++] = t[i++];
       }
@@ -113,7 +130,34 @@ public class Trieur extends Thread {
     }
   }
   public static void main(String[] args) {
-    int[] t = {5, 8, 3, 2, 7, 10, 1};
+    String input = null ;
+    System.out.print("input size array : ");
+    try{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    input = br.readLine();
+ 
+    while(input.length()<=0){
+      System.out.print("input size array : ");
+       br = new BufferedReader(new InputStreamReader(System.in));
+       input = br.readLine();
+    }
+  }catch(IOException io){
+    io.printStackTrace();
+  }
+    int size = 0;
+    try {
+		size = Integer.parseInt(input);
+	} catch (Exception e) {
+		// TODO: handle exception
+		System.out.println("type of your input value is not integer ;;; "+ e);
+	}
+     
+    Random r = new Random();
+    int []  t = new int[size];
+    for(int i=0;i<size;i++){
+    	t[i] = r.nextInt(10);
+    }
+   // int[] t = {5, 8, 3, 2, 7, 10, 1};
     Trieur trieur = new Trieur(t);
     try {
       trieur.join();
@@ -122,6 +166,7 @@ public class Trieur extends Thread {
       for (int i = 0; i <t.length; i++) {
       System.out.print(t[i] + " ; ");
      }
+
     System.out.println("Done");
   }
 
